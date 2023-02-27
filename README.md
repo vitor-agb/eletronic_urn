ffd0046afc05de8045ae068fa794ebc58f0b3f61
Vitor Barbeiro <vitor.agb@hotmail.com>
2023-02-27 07:18:57 -0300

    Send files

---
+++ README.md
@@ -0,0 +1,10 @@
+# About the project
+
+Academic project for the Javascript course at B7Web.
+Creation of an electronic voting urn for the city councilman and mayor elections.
+
+# Technologies used
+## Front end
+- HTML
+- CSS   
+- JavaScript
---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'João das Dores',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Maria da Silva',
+                partido: 'CBA',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano Almeida',
+                partido: 'ABC',
+                vice: 'Machado Assis',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano Fred',
+                partido: 'QWERTY',
+                vice: 'Jane Jon',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
